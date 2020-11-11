40074b1c0e14e3707e4e9939e1ff458dad497e2d
ucpr <contact@ucpr.dev>
2020-11-12 07:10:50 +0900

    feat: expires_inをcacheのインスタンスをを作るときに指定するようにした

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -10,10 +10,10 @@ class MicropostsController < ApplicationController
   # GET /microposts/1
   # GET /microposts/1.json
   def show
-    cache = ActiveSupport::Cache::RedisCacheStore.new
+    cache = ActiveSupport::Cache::RedisCacheStore.new(expires_in: 1.minutes)
 
     id = params[:id]
-    content = cache.fetch("microposts:" + id, expires_in: 1.minutes) do
+    content = cache.fetch("microposts:" + id) do
       @micropost.content
     end
 
